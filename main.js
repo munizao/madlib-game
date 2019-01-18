4c27bb8303dfd5dca295eb566212a5b4bd077d47
Alexandre Muniz <aom@puzzlezapper.com>
2019-01-18 15:30:52 -0800

    added main.css

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function madLib() {
     var bodyPart = document.getElementById('body-part').value;
     var verb = document.getElementById('verb').value;
 
-    story = 'Little ' + adjective + ' ' + clothing + ' wanted to go to her Granmother\'s house. To get there, she had to walk through the ' + location 
+    story = 'Little ' + adjective + ' ' + clothing + ' wanted to go to her Granmother\'s house. To get there, she had to walk through the ' + location + ' On the way there, the Big Bad ' + animal + 'tried to catch her, but she got away. So the Big Bad ' + animal + ' went to Grandmother\'s house instead, ate her, put on her clothes, and got into her ' + noun + '. ';
     var storyElement = document.getElementById('story');
     storyElement.textContent = story;
 }
